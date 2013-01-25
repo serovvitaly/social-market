4d2f2fae5a3cee9198bb7de770d8af8d97675923
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-25 20:27:56 +0400

    Настройка универсального контроллера

--- app/controller/Photo.php
+++ app/controller/Photo.php
@@ -4,6 +4,16 @@ namespace App\Controller;
 
 class Photo
 {
+    
+    
+    public function action_hello()
+    {
+        echo 'CONT';
+        
+        return 'hello';
+    }
+    
+    
     public static function openAction()
     {
         return 'go-go';
--- www/index.php
+++ www/index.php
@@ -7,9 +7,30 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 
-$app->get('/{uri}', 'App\Controller\Photo::openAction')->assert('uri', '.+')->value('uri', 'index/');
+$app->get('/{uri}', function ($uri) use ($app) {
+    
+    $uri_mix  = explode('?', $uri);
+    $uri_path = $uri_mix[0];
+    $uri_mix  = explode('/', $uri_path);
+    
+    $controller_name = (isset($uri_mix[0]) AND !empty($uri_mix[0])) ? $uri_mix[0] : 'index';
+    $controller_name = "App\\Controller\\" . ucfirst( strtolower($controller_name) );
+    $controller      = new $controller_name;
+    
+    $action_name = (isset($uri_mix[1]) AND !empty($uri_mix[1])) ? $uri_mix[1] : 'index';
+    $action_name = 'action_' . strtolower($action_name);
+    
+    $output = $controller->$action_name;
+    
+    var_dump($output);
+    
+    return '<hr/>';    
+    
+})->assert('uri', '.+')->value('uri', 'index/');
 
 /*
+'App\Controller\Photo::openAction'
+
 $app->get('/{uri}', function ($uri) use ($app) {
     return $uri;
 })->assert('uri', '.+')->value('uri', 'index/');
