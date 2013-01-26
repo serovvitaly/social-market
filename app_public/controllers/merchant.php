a822e967b30443b56150eb3335d3677afa2b8f73
Vitaliy Serov <serovvitaly@gmail.com>
2013-01-26 06:34:36 +0400

    сделана универсальная шаблонизация для контроллера merchant

--- app_public/controllers/merchant.php
+++ app_public/controllers/merchant.php
@@ -2,23 +2,32 @@
 
 
 class Merchant_Controller extends Private_Controller
-{
+{   
+    protected $_data = array();
     
+    protected function _layout($template)
+    {
+        $this->_data['user'] = Auth::user();
+        
+        return View::make($template, $this->_data);
+    }
+    
+ 
     public function action_index()
     {
-        return View::make('merchant.index');
+        return $this->_layout('merchant.index');
     }
     
     
     public function action_apps_create()
     {
-        return View::make('merchant.apps.create');        
+        return $this->_layout('merchant.apps.create');        
     }
     
     
     public function action_apps_list()
     {
-        return View::make('merchant.apps.list');        
+        return $this->_layout('merchant.apps.list');        
     }
     
 }
